d903643dfa43760a6d710dfa74d16dacfc0f5ba5
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-05-25 07:48:37 -0500

    feat: :rocket: Script added
    New script for the use of the form

--- Punto 1/main.js
+++ Punto 1/main.js
@@ -1,15 +1,35 @@
-
-class persona{
-    constructor(name, age, gender){
+class Persona {
+    constructor(name, age, gender) {
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
-    saludar(){
-        return `<h3> Buenas, ${this.name} un gusto conocerte </h3>`
+    saludar() {
+        return `Buenas ${this.name}, es un placer conocerte.`;
     }
 }
-let persona1 = new persona('Camilo','19','Male')
 
-console.log(persona1.saludar())
+class Estudiante extends Persona {
+    constructor(name, age, gender, carrera) {
+        super(name, age, gender);
+        this.carrera = carrera;
+    }
+
+    estudiar() {
+        return `Eres estudiante de ${this.carrera}.`;
+    }
+}
+
+const formulario = document.getElementById("MyForm");
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = document.getElementById("nombre").value;
+    const age = document.getElementById("edad").value;
+    const gender = document.getElementById("sexo").value;
+    const carrera = document.getElementById("carrera").value;
+
+    let Usuario = new Estudiante(name, age, gender, carrera);
+    document.getElementById("saludo").textContent = Usuario.saludar();
+    document.getElementById("info-carrera").textContent = Usuario.estudiar();
+});
