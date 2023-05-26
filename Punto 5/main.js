775fde644db2d20024caae577affd5e854098506
JuanCamiloPaezArizaCAMPUS <camipa2305@gmail.com>
2023-05-25 22:32:07 -0500

    feat: :sparkles: New script
    New script for the excercise 5 of the poo js workshop

---
+++ Punto 5/main.js
@@ -0,0 +1,47 @@
+class Empleado {
+    constructor({nombre, edad, sueldo}) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+    }
+    calcularSalarioAnual(){
+        this.sueldo = (this.sueldo*12) 
+        return `El sueldo Anual es: ${this.sueldo}`;
+    }
+    static generarIdEmpleado(){
+        
+    }
+}
+
+class Gerente extends Empleado{
+    constructor({nombre,edad,sueldo,departamento}){
+        super({nombre,edad,sueldo})
+        this.departamento = departamento
+    }
+    calcularSalarioAnualizado(){
+       let bono = (this.salario * 0,1 )
+       this.salario = (this.salario + bono)*12
+       return `El salario del Gerente es: ${this.salario}`
+    }
+}
+
+
+
+
+
+
+
+
+
+
+let form = document.getElementById('MyForm');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    let empleado1 = new Empleado(data);
+    let gerente1 = new Gerente(data);
+
+    document.getElementById('salarioA').textContent = empleado1.calcularSalarioAnual();
+    document.getElementById('salarioB').textContent = gerente1.calcularSalarioAnual();
+    
+})
